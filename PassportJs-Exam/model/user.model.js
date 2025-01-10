184ebf939be50e2c1e98daac21dbbe94042584c9
AMEE451 <desaiamee699@gmail.com>
2025-01-10 23:03:02 +0530

    Passport-js

--- PassportJs-Exam/model/user.model.js
+++ PassportJs-Exam/model/user.model.js
@@ -1,10 +1,29 @@
-const mongoose=require("mongoose")
+const express = require('express');
+const router = express.Router();
+const { body } = require("express-validator")
+const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
-const userSchema=new mongoose.Schema({
-    username:String,
-    email:String,
-    password:Number
-})
 
-const User=mongoose.model("User",userSchema)
-module.exports=User
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.registerUser
+)
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
+
+
+
+module.exports = router;
